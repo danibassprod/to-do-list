9a0069524263337905001f896c48afe1e6394bef
danibassprod <cristianobajista21@gmail.com>
2024-05-16 14:44:21 -0400

    Create default project with a todo

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
-import './css/reset.css'
-import './css/style.css'
+import './css/reset.css';
+import './css/style.css';
+import { CreateProject, CreateToDo } from './logic';
+
+const userProjects = {};
+
+userProjects.defaultProject = new CreateProject(
+    'Default Project', 
+    'You can use this project to test this app!'
+);
+
+userProjects.defaultProject.todos.push(new CreateToDo(
+    'Practice 15 minutes a day typing', 
+    'Use resources like Typing Club', 
+    new Date().getFullYear(), 
+    2025, 
+    3)
+);
+
+console.log(userProjects);
