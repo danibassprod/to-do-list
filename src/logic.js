564bba637a361383fca54b17badf2e063287e712
danibassprod <cristianobajista21@gmail.com>
2024-05-24 09:36:50 -0400

    Add completed and project properties to CreateToDo class

--- src/logic.js
+++ src/logic.js
@@ -1,11 +1,12 @@
 class CreateToDo {
-    constructor(title = 'No title provided.', desc = 'No description provided', date = new Date().getFullYear(), dueDate = 'No due date provided.', priority = 0, completed = false) {
+    constructor(title = 'No title provided.', desc = 'No description provided', date = new Date().getFullYear(), dueDate = 'No due date provided.', priority = 0, completed = false, project) {
         this.title = title,
         this.desc = desc,
         this.date = date,
         this.dueDate = dueDate,
         this.priority = priority,
-        this.completed = completed
+        this.completed = completed,
+        this.project = project
     }
 }
 
