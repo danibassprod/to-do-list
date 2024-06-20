15fda2a843dbf0c3f7cff876aa3cb06a7c24d634
danibassprod <cristianobajista21@gmail.com>
2024-06-20 10:06:26 -0400

    Create two todos for default project

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import './css/reset.css';
 import './css/style.css';
 import { CreateProject, CreateToDo, userProjects } from './logic';
-import { DOMElements } from './dom.js'
 
 userProjects.default = new CreateProject('Default Project', 'Project for test purposes')
 
 userProjects.default.todos.push(new CreateToDo(
     'test title',
     'test desc',
-    '31/12/1231',
-    '42/31/1351',
+    '31-12 -1231',
+    '42-31-1351',
     'high',
+    'default',
+    'no'
+))
+
+userProjects.default.todos.push(new CreateToDo(
+    'test lelelel',
+    'test dasdasd',
+    '31-12-1231',
+    '42-31-1351',
+    'mid',
     'default',
     'no'
 ))
